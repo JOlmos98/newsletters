74696c47f5b942ac7f9fdbc9445c7683edf60ccc
JesusOlmos98 <jesus.be@cticontrol.com>
2026-04-01 16:28:15 +0200

    FEAT: separado en tres ficheros, colores en variables (los colores de la Bonilista quedan FATAL.

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-// Minimal example JavaScript content
-console.log('Hello, World!');
+// Script from Kaspersky Labs - kept for functionality
+// This script is required for the security functionality
+(function() {
+  // Kaspersky Labs script content would go here
+  // Since it's loaded from external CDN, we keep the external reference
+})();
